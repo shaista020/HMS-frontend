4f117761de5fb7700701c2e0160cdace43c1127f
shaista020 <shaistatabbasum523@gmail.com>
2026-01-01 12:11:48 +0500

    Fix User folder case for Vercel

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ import UpdateGuest from './pages/Admin/Guest/Update_guest';
 import AddPayment from './pages/Admin/Payment/add_payment';
 import ListPayment from './pages/Admin/Payment/list_payment';
 import UpdatePayment from './pages/Admin/Payment/update_payment';
-import UserDashboard from './pages/User/pages/UserDashboard';
+import UserDashboard from "./pages/User/pages/UserDashboard";
+
 import RoomType from './pages/User/pages/RoomType';
 import Room from './pages/User/pages/Room';
 
